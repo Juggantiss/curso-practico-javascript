bcaf842db826e82e22c883bc7433b4eaf954345a
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2021-09-09 18:33:55 -0500

    Creado formularios para perimetro y area de triangulo y circulo

--- figures.js
+++ figures.js
@@ -10,7 +10,8 @@ console.groupEnd();
 // Code of triangle
 console.group("Triangle");
 
-const perimeterTriangle = (sideOne, sideTwo, base) => sideOne + sideTwo + base;
+const perimeterTriangle = (sideOne, sideTwo, base) =>
+  sideOne + (sideTwo + base);
 
 const areaTriangle = (base, height) => (base * height) / 2;
 
@@ -29,20 +30,68 @@ console.groupEnd();
 
 // Here to interact with HTML
 
+// Square
 const calculatePerimeterSquare = () => {
-  const input = document.getElementById("InputSquare");
-  const value = input.value;
+  const inputSide = document.getElementById("InputSquare");
+  const side = inputSide.value;
 
-  const perimeter = perimeterSquare(value);
+  const perimeter = perimeterSquare(side);
 
   alert(perimeter);
 };
 
 const calculateAreaSquare = () => {
-  const input = document.getElementById("InputSquare");
-  const value = input.value;
+  const inputSide = document.getElementById("InputSquare");
+  const side = inputSide.value;
 
-  const area = areaSquare(value);
+  const area = areaSquare(side);
+
+  alert(area);
+};
+
+// Triangle
+const calculatePerimeterTriangle = () => {
+  const inputOne = document.getElementById("InputTriangleSideOne");
+  const sideOne = Number(inputOne.value);
+
+  const inputTwo = document.getElementById("InputTriangleSideTwo");
+  const sideTwo = Number(inputTwo.value);
+
+  const inputBase = document.getElementById("InputTriangleBase");
+  const base = Number(inputBase.value);
+
+  const perimeter = perimeterTriangle(sideOne, sideTwo, base);
+
+  alert(perimeter);
+};
+
+const calculateAreaTriangle = () => {
+  const inputBase = document.getElementById("InputTriangleBase");
+  const base = Number(inputBase.value);
+
+  const inputHeight = document.getElementById("InputTriangleHeight");
+  const height = Number(inputHeight.value);
+
+  const area = areaTriangle(base, height);
+
+  alert(area);
+};
+
+// Circle
+const calculatePerimeterCircle = () => {
+  const inputRatio = document.getElementById("InputCircle");
+  const ratio = inputRatio.value;
+
+  const perimeter = perimeterCircle(ratio);
+
+  alert(perimeter);
+};
+
+const calculateAreaCircle = () => {
+  const inputRatio = document.getElementById("InputCircle");
+  const ratio = inputRatio.value;
+
+  const area = areaCircle(ratio);
 
   alert(area);
 };
